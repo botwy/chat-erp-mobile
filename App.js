02810cc0ceccf25a496f9c400118f5ebb217b3ed
botwy <botwy@mail.ru>
2018-03-09 00:45:44 +0300

    init erp-mudule

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import NativeExample from './mobile_components/App';
+import NativeExample from './mobile_components/chat-module/App';
 
 import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import EventSystem from "./mobile_components/EventSystem";
-import reducer from "./mobile_components/reducer";
+import EventSystem from "./mobile_components/chat-module/EventSystem";
+import reducer from "./mobile_components/chat-module/reducer";
 
 
 
